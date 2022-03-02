38d92f2b5f5c341e86b529c44052a50ede04b36b
laloh <eduardocarhue@gmail.com>
2022-03-01 20:33:33 -0600

    Finish backend

--- backend/api/movie.route.js
+++ backend/api/movie.route.js
@@ -5,6 +5,8 @@ import ReviewsController from "./reviews.controller.js";
 const router = express.Router(); // get access to express router
 
 router.route("/").get(MovieController.apiGetMovies);
+router.route("/id/:id").get(MovieController.apiGetMovieById);
+router.route("/ratings").get(MovieController.apiGetRatings);
 
 router
   .route("/review")
--- backend/dao/moviesDAO.js
+++ backend/dao/moviesDAO.js
@@ -1,3 +1,5 @@
+import { ObjectId } from "mongodb";
+
 let movies;
 
 export default class MoviesDAO {
@@ -40,6 +42,42 @@ export default class MoviesDAO {
     } catch (e) {
       console.error(`Unable to issue find command, ${e}`);
       return { movieList: [], totalNumMovies: 0 };
+    }
+  }
+
+  static async getRatings() {
+    let ratings = [];
+    try {
+      ratings = await movies.distinct("rated");
+      return ratings;
+    } catch (e) {
+      console.error(`unable to get ratings, $(e)`);
+      return ratings;
+    }
+  }
+
+  static async getMovieById(id) {
+    try {
+      return await movies
+        .aggregate([
+          {
+            $match: {
+              _id: new ObjectId(id),
+            },
+          },
+          {
+            $lookup: {
+              from: "reviews",
+              localField: "_id",
+              foreignField: "movie_id",
+              as: "reviews",
+            },
+          },
+        ])
+        .next();
+    } catch (e) {
+      console.error(`something went wrong in getMovieById: ${e}`);
+      throw e;
     }
   }
 }
